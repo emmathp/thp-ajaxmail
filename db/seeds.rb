343a171eaf770c7d2bd792d1b9f66c38f1fc59a2
Emma <emmalg.work@gmail.com>
2020-11-19 10:29:17 +0100

    morning commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-3.times do
-  my_category = Category.create(title: Faker::Book.genre)
-  3.times do
-    my_task = Task.new(title: Faker::Book.title,
-                       deadline: Faker::Date.forward(23),
-                       image: Faker::Avatar.image)
-    my_task.category = my_category
-    my_task.save
-  end
+5.times do
+  Email.create(object: Faker::Beer.name, body: Faker::ChuckNorris.fact)
 end
+
 puts "seeding db"
